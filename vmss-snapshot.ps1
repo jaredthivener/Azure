29d067a844901d49087554820cc745f4a37f9502
Jared Thivener <87688021+jaredthivener@users.noreply.github.com>
2021-08-25 11:10:51 -0600

    Add files via upload

---
+++ vmss-snapshot.ps1
@@ -0,0 +1,10 @@
+$rgname = "rg-arrow-logstash-prod-westus"
+$vmssname = "logstashss-vmss"
+$Id = 1
+$location = "West US"
+
+$vmss1 = Get-AzVmssVM -ResourceGroupName $rgname -VMScaleSetName $vmssname -InstanceId $Id     
+
+$snapshotconfig = New-AzSnapshotConfig -Location $location -AccountType Standard_LRS -OsType Linux -CreateOption Copy -SourceUri $vmss1.StorageProfile.OsDisk.ManagedDisk.id
+
+New-AzSnapshot -ResourceGroupName $rgname -SnapshotName 'mySnapshot' -Snapshot $snapshotconfig
