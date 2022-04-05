0ed8a1821304e9bf3366bbfb7ae7fa16b8640b58
Jared Thivener <87688021+jaredthivener@users.noreply.github.com>
2022-04-05 15:29:23 -0600

    Update Start-MultipleVMs.ps1

--- Start-MultipleVMs.ps1
+++ Start-MultipleVMs.ps1
@@ -1,5 +1,5 @@
 "vm1","vm2","vm3","vm4","vm5" | Foreach-Object {
   Start-Job -ScriptBlock {
-      Start-AzVM -ResourceGroupName <rg-name> -Name $using:PSItem -Force
+      Start-AzVM -ResourceGroupName <rg-name> -Name $using:PSItem -NoWait
     }
 }    
